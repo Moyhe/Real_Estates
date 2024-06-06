323a033be8b75001093a84f111462b1a58752dde
Mohye <dragonsinmeliodas777@gmail.com>
2024-06-06 19:58:22 +0300

    configure deployment files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\User;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
@@ -16,7 +17,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        $adminUser = User::factory()->create([
+            'email' => 'Geni@gmail.com',
+            'name' => 'Geni',
+            'password' => Hash::make('kenkanekiTouka123')
+        ]);
+
+        $adminRole = Role::create(['name' => 'Admin']);
+        $adminUser->assignRole($adminRole);
+
         \App\Models\EstateMarket::factory(10)->create();
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
